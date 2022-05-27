92811cd838bbd27648771a8f1181c646964e5f2a
Musa Dauda <marshalmusa143@gmail.com>
2022-05-27 11:23:16 +0100

    Restructured the files and folder

--- python/main.py
+++
@@ -1,9 +0,0 @@
-word = input("Enter a word you would like to count... Whitespace is ignored: ")
-sum = 0
-
-for i in word:
-    if i == ' ':
-        pass
-    else:
-        sum += 1
-print(f"There are {sum} alphabet in the word: \"{word}\" excluding whitespaces")
