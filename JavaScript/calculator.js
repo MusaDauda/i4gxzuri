50d4ddb671204df0e6b3aa385de35b7fb1a3d1bf
Musa Dauda <marshalmusa143@gmail.com>
2022-06-28 10:08:32 +0100

    beautify with comments, lol

--- JavaScript/calculator.js
+++ JavaScript/calculator.js
@@ -1,1 +1,34 @@
-console.log("I will be done with this in a giffy, am facing phone issues, no excuse though!")
+// Simple calculator in javaScript
+
+// Ask for operator input
+var operator = prompt("Enter an operator: +, -, *, /, %(modulo)");
+
+//Take operand input
+var operarand1 = parseFloat(prompt("Enter first operand"));
+var operarand2 = parseFloat(prompt("Enter second operand"));
+
+// find addition, subtraction, division and multiplication using a variable: result
+var result;
+// using if...else expression
+if (operator == "+") {
+    result = operarand1 + operarand2;
+}
+else if (operator == "-") {
+    result = operarand1 - operarand2;
+}
+else if (operator == "*") {
+    result = operarand1 * operarand2;
+}
+else if (operator  == "/") {
+    result = operarand1 / operarand2;
+}
+else if (operator == "%") {
+    result = operarand1 % operarand2;
+}
+else {
+    alert("Invalid operation");
+}
+
+// Display the result
+console.log(`${operarand1} ${operator} ${operarand2} = ${result}`);
+alert(`${operarand1} ${operator} ${operarand2} = ${result}`);
