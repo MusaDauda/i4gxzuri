b155d188d44ec3cefb2a395f196cc9e5cc766b40
Musa Dauda <marshalmusa143@gmail.com>
2022-06-28 13:30:11 +0100

    Completed all task and tested all CRUD functionality

---
+++ Django Projects/Django CRUD/MarshalMusa/blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+# from blog.models import title, body, publish, author, slug, created, updated, status
+from blog.models import Post
+# Register your models here.
+# admin.site.register(body)
+# admin.site.register(title)
+# admin.site.register(publish)
+# admin.site.register(author)
+# admin.site.register(slug)
+# admin.site.register(created)
+# admin.site.register(updated)
+# admin.site.register(status)
+
+admin.site.register(Post)
---
+++ Django Projects/Django CRUD/README.md
@@ -0,0 +1,2 @@
+## Tested and passed all CRUD functionality.
+![Image of working Project](django_crud.png "Project Image")
